d12341903bab018ae505575b49294525019784c5
Des Bro <lockhaim@gmail.com>
2025-08-08 21:07:50 -0600

    template literal solution

---
+++ JavaScript/Fundamental/returningStrings.js
@@ -0,0 +1,6 @@
+//Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+
+function greet(name){
+  var greeting = `Hello, ${name} how are you doing today?`
+  return greeting
+}
