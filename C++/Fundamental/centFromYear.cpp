80e863137fcebd8f075157c6aa6577305f40c411
Des Bro <lockhaim@gmail.com>
2025-08-19 09:53:55 -0600

    centFromYear missing soln and double int. probably start doing harder ones these a monkey could do

---
+++ C++/Fundamental/centFromYear.cpp
@@ -0,0 +1,13 @@
+/*
+Introduction
+The first century spans from the year 1 up 
+to and including the year 100, the second 
+century - from the year 101 up to and including
+the year 200, etc.
+
+Given a year, return the century it is in.
+*/
+
+int centuryFromYear(int year) {
+    return (year - 1) / 100 + 1;
+}
